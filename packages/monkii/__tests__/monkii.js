6c3bee43c1d690dcf47ca905b77799875dc9f8cb
Ratson <github@ratson.name>
2018-01-03 14:03:17 +0800

    Transform class properties

--- packages/monkii/__tests__/monkii.js
+++ packages/monkii/__tests__/monkii.js
@@ -1,7 +1,15 @@
 'use strict'
 
+const execa = require('execa')
+const globby = require('globby')
+
 const monkii = require('..')
 
 test('export factory function', () => {
   expect(monkii).toBeInstanceOf(Function)
+})
+
+test('run by node', async () => {
+  const files = await globby('../lib/*.js', { cwd: __dirname, absolute: true })
+  await Promise.all(files.map(file => execa('node', [file])))
 })
